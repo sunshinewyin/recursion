5a5707eb4ca5c4f252daae52fe738babefa55ec5
Sunshine Yin <sunshine.w.yin@gmail.com>
2015-02-24 20:27:57 -0800

    Added recursion for arrays and for object properties

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,21 +4,30 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function (obj) {
-  var str = '';
+  var str = "";
   var numOfKeysInObj = Object.keys(obj).length;
+  var arrayLength = obj.length;
 
     if (
       typeof obj === "number" ||
-      typeof obj === null ||
+      // typeof obj === null ||
       typeof obj === "boolean" ||
       typeof obj === "string"
       ) {
 
-      str += obj;
+      str += obj.toString();
+      return str;
 
     } else if (Array.isArray(obj)) {
 
-      str = str + "[" + obj.toString() + "]";
+        if (arrayLength === 0) {
+          return "[]";
+        } else {
+          for (var i=0; i<arrayLength; i++) {
+            str = str + stringifyJSON(obj[i]) + ",";
+          }
+          str = "[" + str + "]";
+        }
 
     } else if (typeof obj === "object") {
 
@@ -27,7 +36,7 @@ var stringifyJSON = function (obj) {
 
         } else {
             for (var p in obj) {
-            str = str + p + ":" + stringifyJSON(obj[p]) + ",";
+            str = str + stringifyJSON(p) + ":" + stringifyJSON(obj[p]) + ",";
             numOfKeysInObj--;
             }
             str = "{" + str + "}";
@@ -37,4 +46,4 @@ var stringifyJSON = function (obj) {
     return str;
 };
 
-//to do: (1) figure out how to get rid of trailing commas; (2) terminate for unstringfiableValues; (3) make sure array within arrays works
+//to do: (1) figure out how to get rid of trailing commas; (2) terminate for unstringfiableValues; (3) make sure array within arrays works, (4) what's up with null
