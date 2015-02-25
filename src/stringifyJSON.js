abc83879d6a11fba9eb2da71ca989e94dec628f9
Sunshine Yin <sunshine.w.yin@gmail.com>
2015-02-24 19:18:41 -0800

    Initial implementation, still some kinks to work out

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,38 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+  var str = '';
+  var numOfKeysInObj = Object.keys(obj).length;
+
+    if (
+      typeof obj === "number" ||
+      typeof obj === null ||
+      typeof obj === "boolean" ||
+      typeof obj === "string"
+      ) {
+
+      str += obj;
+
+    } else if (Array.isArray(obj)) {
+
+      str = str + "[" + obj.toString() + "]";
+
+    } else if (typeof obj === "object") {
+
+        if (numOfKeysInObj === 0) {
+            return "{}";
+
+        } else {
+            for (var p in obj) {
+            str = str + p + ":" + stringifyJSON(obj[p]) + ",";
+            numOfKeysInObj--;
+            }
+            str = "{" + str + "}";
+        }
+    }
+
+    return str;
 };
+
+//to do: (1) figure out how to get rid of trailing commas; (2) terminate for unstringfiableValues; (3) make sure array within arrays works
