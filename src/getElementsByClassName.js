16d8ea524bf3e0a70bbd4f1879a68d14faf7377c
Sunshine Yin <sunshine.w.yin@gmail.com>
2015-03-03 15:07:20 -0800

    Complete implementation

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,81 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+
+	var arrayOfNodesWithClassName = [];
+	var element = document.body;
+
+	if (element.classList.contains(className)) {
+		arrayOfNodesWithClassName.push(element);
+	}
+
+	var checkNodesOfChildren = function (element) {
+		for (var i = 0; i<element.childNodes.length; i++) {
+			// var child = element.childNodes[i];
+			if (element.childNodes[i].nodeType == document.ELEMENT_NODE) {
+				if (element.childNodes[i].classList.contains(className)) {
+					arrayOfNodesWithClassName.push(element.childNodes[i]);
+				}
+			checkNodesOfChildren(element.childNodes[i]);
+			}
+		}
+	};
+
+	checkNodesOfChildren(element);
+	return arrayOfNodesWithClassName;
 };
+
+
+
+
+//
+// 			if (element.childNodes.length>0) {
+
+
+// 			}
+// 		} else if (element)
+
+// 	}
+
+// 	if (element.length === 1) {
+// 		if (element.classList.contains(className)) {
+
+
+
+
+// 	var getElementsForEachChild = function (element)
+
+// 		for (var i = 0; i<element.length; i
+
+
+
+// 		} else if (element.length > 1) {
+// 			for (var i = 0; i<element.length; i++) {
+
+
+// 			}
+// 		}
+
+
+// 	}
+
+
+
+
+// 	 {
+
+// 	if (element.childNodes.length === 1 && element.childNodes[i] == document.TEXT_NODE) {
+// 		if (element.childNodes[i].classList.contains(className)) {
+// 			arrayOfNodesWithClassName.push(element.childNodes[i]);
+// 			}
+// 			return arrayOfNodesWithClassName;
+
+// 		} else if (element.childNodes.length > 1 && element.childNodes[i] == document.ELEMENT_NODE) {
+// 			for (var i= 0; i<element.childNodes.length; i++) {
+// 				if (element.childNodes[i].classList.contains(className)) {
+// 					arrayOfNodesWithClassName.push(element.childNodes[i]);
+// 				}
+
+// 	return arrayOfNodesWithClassName;
+// };
